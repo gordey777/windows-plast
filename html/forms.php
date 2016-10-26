ece12df078a298711cb3e556f6a959790f36dcca
Oleg Gordiyenko <oleg.gordey777@gmail.com>
2016-10-26 22:38:53 +0300

    styles in one file and del trash

--- html/forms.php
+++ html/forms.php
@@ -1,5 +1,5 @@
 <?
-if((isset($_POST['name'])&&$_POST['name']!="")&&(isset($_POST['phone'])&&$_POST['phone']!="")){ //Проверка отправилось ли наше поля name и не пустые ли они
+if((isset($_POST['name'])&&$_POST['name']!="")&&(isset($_POST['tel'])&&$_POST['tel']!="")){ //Проверка отправилось ли наше поля name и не пустые ли они
         $to = 'mail@yandex.ru'; //Почта получателя, через запятую можно указать сколько угодно адресов
         $subject = 'Обратный звонок'; //Загаловок сообщения
         $message = '
@@ -9,7 +9,8 @@ if((isset($_POST['name'])&&$_POST['name']!="")&&(isset($_POST['phone'])&&$_POST[
                     </head>
                     <body>
                         <p>Имя: '.$_POST['name'].'</p>
-                        <p>Телефон: '.$_POST['phone'].'</p>
+                        <p>Телефон: '.$_POST['tel'].'</p>
+                        <p>Телефон: '.$_POST['type'].'</p>
                     </body>
                 </html>'; //Текст нащего сообщения можно использовать HTML теги
         $headers  = "Content-type: text/html; charset=utf-8 \r\n"; //Кодировка письма
