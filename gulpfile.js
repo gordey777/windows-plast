967497e007193dd125e7e52d7d7fe623f7b080ae
crazyyy <crazyyy@gmail.com>
2016-10-26 22:05:40 +0300

    bf domained

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,6 @@
 /* if work with html set TRUE, else - FALSE */
-var htmlOWp = true,
-  // set wordpress template folder
-  wpThemeName = 'wp-framework',
-  // and set wordpress domain
-  wpDomain = 'wp-framework.dev';
+var htmlOWp = false,
+  wpDomain = 'windowsplast.dev';
 
 var AUTOPREFIXER_BROWSERS = [
   'ie >= 8', 'ie_mob >= 10', 'ff >= 20', 'chrome >= 24', 'safari >= 5', 'opera >= 12', 'ios >= 7', 'android >= 2.3', '> 1%', 'last 4 versions', 'bb >= 10'
@@ -206,22 +203,12 @@ gulp.task('cache:clear', function(done) {
 
 // Browser Sync
 gulp.task('browserSync', function() {
-  if (htmlOWp === true) {
-    browserSync({
-      notify: false,
-      port: 9080,
-      server: {
-        baseDir: basePaths.dest,
-      }
-    });
-  } else {
-    browserSync({
-      notify: false,
-      proxy: wpDomain,
-      host: wpDomain,
-      port: 9090
-    });
-  }
+  browserSync({
+    notify: false,
+    proxy: wpDomain,
+    host: wpDomain,
+    port: 9090
+  });
 });
 
 gulp.task('styles', function(callback) {
